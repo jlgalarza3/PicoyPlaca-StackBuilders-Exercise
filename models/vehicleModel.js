20b21c7b1145d1c9591dfc289d6cb5986b5a8748
Jorge Galarza <jorge2000cruz@gmail.com>
2022-07-18 17:39:40 -0500

    Added vehicle Model

---
+++ models/vehicleModel.js
@@ -0,0 +1,13 @@
+// Vehicle Model
+// ID: Number
+// Licence Plate Number: String
+class Vehicle {
+  constructor(id, licencePlateNumber) {
+    this.id = id;
+    this.licencePlateNumber = licencePlateNumber;
+  }
+}
+
+module.exports = {
+  Vehicle,
+};
