12450f4e38342eeb5fe1abee6b1acedcc294eb6d
Jorge Galarza <jorge2000cruz@gmail.com>
2022-07-19 15:03:28 -0500

    Unit testing: Thursday and friday cases

--- __tests__/test.js
+++ __tests__/test.js
@@ -55,3 +55,35 @@ describe("Wednesday cases with licence plate number 5-6", () => {
     }
   );
 });
+
+//Must return the correct message on thursday cases
+const thursdayCases = [
+  ["PBC-8737", "07/21/2022", "17:30", "Can not circulate"],
+  ["PBC-8738", "07/21/2022", "19:15", "Can circulate"],
+  ["PBC-8730", "07/21/2022", "18:00", "Can circulate"],
+];
+describe("Thursday cases with licence plate number 7-8", () => {
+  test.each(thursdayCases)(
+    "Given the vehicle with licence plate number %p at %p at %p, the vehicle: %p",
+    (licencePlateNumber, date, time, expectedResult) => {
+      const result = calculatePicoyPlaca(licencePlateNumber, date, time);
+      expect(result).toEqual(expectedResult);
+    }
+  );
+});
+
+//Must return the correct message on friday cases
+const fridayCases = [
+  ["PBC-8739", "07/22/2022", "16:03", "Can not circulate"],
+  ["PBC-8730", "07/22/2022", "20:15", "Can circulate"],
+  ["PBC-8735", "07/22/2022", "13:00", "Can circulate"],
+];
+describe("Friday cases with licence plate number 9-0", () => {
+  test.each(fridayCases)(
+    "Given the vehicle with licence plate number %p at %p at %p, the vehicle: %p",
+    (licencePlateNumber, date, time, expectedResult) => {
+      const result = calculatePicoyPlaca(licencePlateNumber, date, time);
+      expect(result).toEqual(expectedResult);
+    }
+  );
+});
